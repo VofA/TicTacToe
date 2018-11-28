db82bebcf9a3f312a6bf7fc51460f84c0fdf3fb5
Anton Chushialov <antonn.chushialov@gmail.com>
2018-11-28 19:30:39 +0400

    Second version

--- main.cpp
+++ main.cpp
@@ -1,114 +1,128 @@
 #include <iostream>
+#include <string>
+#include <ncurses.h>
 
 int field[3][3];
 
-bool error(int x, int y) {
-    if (x < 0 || x > 2 || y < 0 || y > 2) {
-        std::cout << "Repeat enter\n";
-        return true;
-    } else {
-        return false;
-    }    
+const int PLAYER_X = 1;
+const int PLAYER_O = 0;
+std::string getPlayer(int player) {
+  switch (player) {
+    case PLAYER_X:
+      return "x";
+    case PLAYER_O:
+      return "o";
+    default:
+      return " ";
+  }
 }
 
-void draw() {
-    
-    for(int i = 0; i < 3; i++)
-    {
-        
-        for(int j = 0; j < 3; j++)
-        {
-            if (field[i][j] != 1 && field[i][j] != 0) {
-                std::cout << " ";
-                if (j != 2) {
-                    std::cout << "|";
-                }
-            } else {
-                
-                switch (field[i][j])
-                {
-                    case 0:
-                        std::cout << "o";
-                        break;
-                       
-                
-                    default:
-                        std::cout <<  "x";    
-                        break;
-                }
-                if (j != 2) {
-                    std::cout << "|";
-                }
-            }
-        }
-        std::cout << "\n";
-    }
-    
+void draw(int coordX, int coordY) {
+  clear();
+
+  for (int y = 0; y < 3; ++y) {
+    for (int x = 0; x < 3; ++x) {
+      std::string out;
+      if (coordX == x && coordY == y) {
+        out = "[" + getPlayer(field[y][x]) + "]";
+      } else {
+        out = " " + getPlayer(field[y][x]) + " ";
+      }
+      addstr(out.c_str());
+      
+      if (x != 2) {
+        addstr("|");
+      }
+     }
+    addstr("\n");
+  }
 }
 
 bool checkWinner() {
+  return ((field[0][0] == field[0][1] && field[0][1] == field[0][2]) ||
+          (field[1][0] == field[1][1] && field[1][1] == field[1][2]) ||
+          (field[2][0] == field[2][1] && field[2][1] == field[2][2]) ||
 
-    if ((field[0][0] == field[0][1] && field[0][1] == field[0][2]) || 
-        (field[1][0] == field[1][1] && field[1][1] == field[1][2]) || 
-        (field[2][0] == field[2][1] && field[2][1] == field[2][2]) || 
+          //vertical lines
+          (field[0][0] == field[1][0] && field[1][0] == field[2][0]) ||
+          (field[0][1] == field[1][1] && field[1][1] == field[2][1]) ||
+          (field[0][2] == field[1][2] && field[1][2] == field[2][2]) ||
 
-        //vertical lines
-        (field[0][0] == field[1][0] && field[1][0] == field[2][0]) || 
-        (field[0][1] == field[1][1] && field[1][1] == field[2][1]) || 
-        (field[0][2] == field[1][2] && field[1][2] == field[2][2]) ||
-        
-        //x
-        (field[0][0] == field[1][1] && field[1][1] == field[2][2]) || 
-        (field[2][0] == field[1][1] && field[1][1] == field[0][2])
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-
+          //x
+          (field[0][0] == field[1][1] && field[1][1] == field[2][2]) ||
+          (field[2][0] == field[1][1] && field[1][1] == field[0][2]));
 }
 
 int main() {
-    int x, y;
-    const int PLAYER_1 = 1, PLAYER_2 = 0;
+  initscr();
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            field[i][j] = 3 * i + j + 2;
-        }
+  for (int y = 0; y < 3; ++y) {
+    for (int x = 0; x < 3; ++x) {
+      field[y][x] = 3 * y + x + 2;
     }
-    bool game = true, pl = true;
+  }
+  draw(1, 1);
 
-    while (game) {
-        std::cin >> x >> y;
-        if (error(x, y)) {
-            continue;
-        }
-        if (field[x][y] == 1 || field[x][y] == 0) {
-            std::cout << "Already taken\n";
-            continue;
-        }
-        if (pl == true) {
-            field[x][y] = PLAYER_1;
-            draw();
-            pl = false;
-        } else {
-            field[x][y] = PLAYER_2;
-            draw();
-            pl = true;
-        }
-        
-        if (checkWinner()) {
-            if (pl == false) {
-                std::cout << "player 1 won\n";
-                game = false;
-            } else {
-                std::cout << "player 2 won\n";
-                game = false;
-            }
-        } 
-        
+  bool currentPlayer = PLAYER_X;
+
+  int x = 1, y = 1;
+
+  while (true) {
+    char key = getch();
+
+    int relativeX = 0;
+    int relativeY = 0;
+
+    bool enter = false;
+
+    switch (key) {
+    case 'w':
+      relativeY--;
+      break;
+    case 'a':
+      relativeX--;
+      break;
+    case 's':
+      relativeY++;
+      break;
+    case 'd':
+      relativeX++;
+      break;
+    case (char)10:
+      enter = true;
+      break;
     }
 
-    return 0;
+    if (x + relativeX >= 0 && x + relativeX <= 2 &&
+        y + relativeY >= 0 && y + relativeY <= 2
+    ) {
+      y += relativeY;
+      x += relativeX;
+    }
+
+    if (enter) {
+      if (field[y][x] < 2) {
+        addstr("Already taken\n");
+        continue;
+      }
+
+      field[y][x] = currentPlayer;
+
+      if (checkWinner()) {
+        break;
+      }
+
+      currentPlayer = !currentPlayer;
+    }
+
+    draw(x, y);
+  }
+
+  endwin();
+
+  if (checkWinner()) {
+    std::cout << "Win: " << getPlayer(currentPlayer) << "\n";
+  }
+
+  return 0;
 }
