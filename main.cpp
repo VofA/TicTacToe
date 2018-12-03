0e88b5ca52e60bffaf429abf4b27f79765c85716
Anton Chushialov <antonn.chushialov@gmail.com>
2018-12-03 10:44:36 +0400

    horizontal work

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <ncurses.h>
 #include <string>
 
-
-
 const int PLAYER_X = 1;
 const int PLAYER_O = 0;
 std::string getPlayer(int player) {
@@ -56,61 +54,42 @@ bool checkWinner(int **field,
                  int cx,
                  int cy) {
 
-  // vert
-  std::string out  = "start = " + std::to_string(cy + (n - 1)) + "\n";
-  out  += "end = " + std::to_string(cy + (n - 1)) + "\n";
-  out  += "sb = " + std::to_string(nilIfNegative(cy - (n - 1))) + "\n";
-  out  += "eb = " + std::to_string(numberIfMax(cy + (n - 1), height - 1) - n + 1) + "\n";
-    addstr(out.c_str());
   for (int y = nilIfNegative(cy - (n - 1));
        y <= numberIfMax(cy + (n - 1), height - 1) - n + 1;
-       ++y) { //(1+2*(n-1))
+       ++y) {
     int checkElement = field[y][cx];
-    out  = "y = " + std::to_string(y) + "\n";
-    addstr(out.c_str());
 
     bool result = true;
     for (int c = 1; c < n; ++c) {
-      out = "c = " + std::to_string(c) + "";
-      addstr(out.c_str());
+
       if (field[y + c][cx] != checkElement) {
         result = false;
         break;
       }
-      out = "y+c = " + std::to_string(c + y) + "";
-      addstr(out.c_str());
     }
-    out = "r = " + std::to_string(result) + "\n";
-    addstr(out.c_str());
+    if (result) {
+      return true;
+    }
+  }
+  for (int x = nilIfNegative(cx - (n - 1));
+       x <= numberIfMax(cx + (n - 1), width - 1) - n + 1;
+       ++x) {
+    int checkElement = field[cy][x];
+
+    bool result = true;
+    for (int c = 1; c < n; ++c) {
+
+      if (field[cy][x + c] != checkElement) {
+        result = false;
+        break;
+      }
+    }
+
     if (result) {
       return true;
     }
   }
 
-  /*      // vertical lines
-  for (int i = 0; i < height - 1; ++i) {
-    for (int j = 0; j < width - 1; ++j) {
-      if (field[j][i] == field[j + 1][i]) {
-        vert++;
-      }
-    }
-    if (vert == numberToWin ) {
-            return true;
-          }
-    vert = 1;
-  } */
-
-  // x
-  /* if (field[i][j] == field[i + 1][j + 1]) {
-     x1++;
-   } else {
-     x1 = 0;
-   }
-   if (field[i][width - 2 - j] == field[i + i][width - 3 - j]) {
-     x2++;
-   } else {
-     x2 = 0;
-   } */
   return false;
 }
 
